579e457133165710f0ce54ae940c7226a2fde99c
vishva goswami <139674987+VishwaGoswami1407@users.noreply.github.com>
2024-08-02 14:54:31 +0530

    commit

---
+++ .history/2667_Create_Hello_World_Function_20240802145344.js
@@ -0,0 +1,20 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+       return "Hello World" 
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+
+```javascript
+output = "Hello World
+
+```
